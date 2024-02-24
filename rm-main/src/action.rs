ff19721f311c578b5b677dcde480c033312bf081
Remigiusz Micielski <rmicielski@purelymail.com>
2024-02-24 15:25:14 +0100

    simplify keycode_to_action

--- rm-main/src/action.rs
+++ rm-main/src/action.rs
@@ -43,28 +43,24 @@ pub fn event_to_action(mode: Mode, event: Event) -> Option<Action> {
         Event::Error => todo!(),
         Event::Render => Some(Action::Render),
         Event::Key(key) if matches!(mode, Mode::Input) => Some(Action::Input(key)),
-        // TODO: simplify this
-        Event::Key(_) => keycode_to_action(event),
+        Event::Key(key) => keycode_to_action(key),
     }
 }
 
-fn keycode_to_action(event: Event) -> Option<Action> {
-    if let Event::Key(key) = event {
-        return match key.code {
-            KeyCode::Tab => Some(Action::ChangeFocus),
-            KeyCode::Char('j') | KeyCode::Down => Some(Action::Down),
-            KeyCode::Char('k') | KeyCode::Up => Some(Action::Up),
-            KeyCode::Char('q') => Some(Action::Quit),
-            KeyCode::Char('?') => Some(Action::ShowHelp),
-            KeyCode::Char('t') => Some(Action::ShowStats),
-            KeyCode::Char('/') => Some(Action::Search),
-            KeyCode::Char('m') => Some(Action::AddMagnet),
-            KeyCode::Char(n @ '1'..='9') => {
-                Some(Action::ChangeTab(n.to_digit(10).expect("This is ok") as u8))
-            }
-            KeyCode::Enter => Some(Action::Confirm),
-            _ => None,
-        };
-    }
-    None
+fn keycode_to_action(key: KeyEvent) -> Option<Action> {
+    return match key.code {
+        KeyCode::Tab => Some(Action::ChangeFocus),
+        KeyCode::Char('j') | KeyCode::Down => Some(Action::Down),
+        KeyCode::Char('k') | KeyCode::Up => Some(Action::Up),
+        KeyCode::Char('q') => Some(Action::Quit),
+        KeyCode::Char('?') => Some(Action::ShowHelp),
+        KeyCode::Char('t') => Some(Action::ShowStats),
+        KeyCode::Char('/') => Some(Action::Search),
+        KeyCode::Char('m') => Some(Action::AddMagnet),
+        KeyCode::Char(n @ '1'..='9') => {
+            Some(Action::ChangeTab(n.to_digit(10).expect("This is ok") as u8))
+        }
+        KeyCode::Enter => Some(Action::Confirm),
+        _ => None,
+    };
 }
