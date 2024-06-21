55b21bbb0fd96cb654a5c8435f525eb7d7b35e4e
Ryan Aidan <aidan@u.nus.edu>
2024-06-21 17:21:48 +0800

    fix: display actual number of torrents in table (#22)
    so it displays the correct number for example when searching/filtering

--- rm-main/src/ui/tabs/torrents/bottom_stats.rs
+++ rm-main/src/ui/tabs/torrents/bottom_stats.rs
@@ -37,7 +37,6 @@ impl BottomStats {
 impl Component for BottomStats {
     fn render(&mut self, f: &mut Frame, rect: Rect) {
         if let Some(stats) = &*self.stats.lock().unwrap() {
-            let all = stats.torrent_count;
             let download = bytes_to_human_format(stats.download_speed);
             let upload = bytes_to_human_format(stats.upload_speed);
 
@@ -50,9 +49,16 @@ impl Component for BottomStats {
 
             let table_manager = &*self.table_manager.lock().unwrap();
             let table = table_manager.table.borrow();
+            let all = table.get_len();
+
             if let Some(current) = table.state.borrow().selected() {
-                let current_idx = current + 1;
-                text = format!(" {current_idx}/{all} | {text}");
+                if all > 0 {
+                    let current_idx = current + 1;
+                    text = format!(" {current_idx}/{all} | {text}");
+                } else {
+                    // dont display index if no items in table
+                    text = format!(" {all} | {text}");
+                }
             } else {
                 text = format!(" {all} | {text}");
             }
