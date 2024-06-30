2fcd7c7166fe33f89567113542c5ab2aa8029074
Ryan Aidan <aidan@u.nus.edu>
2024-06-30 23:23:02 +0800

    feat: fetch rss command (#42)
    * feat: add all torrents fetched by provided rss url
    
    * feat: fetch rss w regex filter
    
    * fix: download all torrents from rss with no filter
    
    * fix: nits
    
    * fix: bail
    
    * fix: nit

--- rm-main/src/cli.rs
+++ rm-main/src/cli.rs
@@ -1,8 +1,9 @@
 use std::{fs::File, io::Read};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use base64::Engine;
 use clap::{Parser, Subcommand};
+use regex::Regex;
 use rm_config::Config;
 use transmission_rpc::types::TorrentAddArgs;
 
@@ -18,11 +19,13 @@ pub struct Args {
 #[derive(Subcommand)]
 pub enum Commands {
     AddTorrent { torrent: String },
+    FetchRss { url: String, filter: Option<String> },
 }
 
 pub async fn handle_command(config: &Config, command: Commands) -> Result<()> {
     match command {
         Commands::AddTorrent { torrent } => add_torrent(config, torrent).await?,
+        Commands::FetchRss { url, filter } => fetch_rss(config, &url, filter.as_deref()).await?,
     }
     Ok(())
 }
@@ -63,5 +66,42 @@ async fn add_torrent(config: &Config, torrent: String) -> Result<()> {
 
         std::process::exit(1);
     };
+    Ok(())
+}
+
+async fn fetch_rss(config: &Config, url: &str, filter: Option<&str>) -> Result<()> {
+    let mut transclient = transmission::utils::client_from_config(&config);
+    let content = reqwest::get(url).await?.bytes().await?;
+    let channel = rss::Channel::read_from(&content[..])?;
+    let re: Option<Regex> = {
+        if let Some(filter_str) = filter {
+            let res = Regex::new(filter_str)?;
+            Some(res)
+        } else {
+            None
+        }
+    };
+    let items = channel.items().iter().filter_map(|item| {
+        if let (Some(title), Some(url)) = (item.title(), item.link()) {
+            if let Some(re) = &re {
+                if re.is_match(title) {
+                    return Some((title, url));
+                }
+            } else {
+                return Some((title, url));
+            }
+        }
+        None
+    });
+    for (title, url) in items {
+        println!("downloading {title}");
+        let args = TorrentAddArgs {
+            filename: Some(url.to_string()),
+            ..Default::default()
+        };
+        if let Err(e) = transclient.torrent_add(args).await {
+            bail!("error while adding a torrent: {e}")
+        }
+    }
     Ok(())
 }
