4cb3c0ece502d820ddcb7a597f164bd50ec1c8c6
micielski <73398428+micielski@users.noreply.github.com>
2024-07-08 18:18:11 +0000

    fix: allow not specifying headers in torrents tab config (#60)

--- rm-config/src/main_config.rs
+++ rm-config/src/main_config.rs
@@ -115,20 +115,25 @@ impl Header {
 
 #[derive(Deserialize)]
 pub struct TorrentsTab {
+    #[serde(default = "default_headers")]
     pub headers: Vec<Header>,
+}
+
+fn default_headers() -> Vec<Header> {
+    vec![
+        Header::Name,
+        Header::SizeWhenDone,
+        Header::Progress,
+        Header::Eta,
+        Header::DownloadRate,
+        Header::UploadRate,
+    ]
 }
 
 impl Default for TorrentsTab {
     fn default() -> Self {
         Self {
-            headers: vec![
-                Header::Name,
-                Header::SizeWhenDone,
-                Header::Progress,
-                Header::Eta,
-                Header::DownloadRate,
-                Header::UploadRate,
-            ],
+            headers: default_headers(),
         }
     }
 }
